8df9350c0812f930091e1576b1ae2536904080ce
fbergaut <f.bergaut@gmail.com>
2021-10-15 09:54:55 +0200

    Installation sequelize + body-parser + mysql2 et démarrage config sequelize

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const userRoutes = require('./routes/userRoutes');
 require('dotenv').config({ path: './config/.env' });
 const app = express();
+
+//routes
+app.use('/api/user', userRoutes);
 
 // server
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port ${process.env.PORT}`);
+
 });
