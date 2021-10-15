0ee21c565c984bab1b48f5e6cac64bdf6db24c26
fbergaut <f.bergaut@gmail.com>
2021-10-15 10:20:41 +0200

    Connexion Sequelize établie dans userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,20 @@
 const router = require('express').Router();
+const { Sequelize } = require("sequelize");
 
+// Connexion Sequelize
+const sequelize = new Sequelize("groupomania", "root", "root", {
+    host: "localhost",
+    dialect: "mysql",
+});
+
+(async() => {
+    try {
+        await sequelize.authenticate();
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+})();
 
 
 
