410ce7e4a0cb36ebde78c1341455c623fc5c8604
fbergaut <f.bergaut@gmail.com>
2021-10-19 09:11:24 +0200

    Corresctif Sequelize après comment avec mentor => reinitialization sequelize + creation user model + creation route post /users

---
+++ migrations/20211018140340-create-user.js
@@ -0,0 +1,54 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        type: Sequelize.STRING
+      },
+      lastname: {
+        type: Sequelize.STRING
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      picture: {
+        type: Sequelize.STRING
+      },
+      bio: {
+        type: Sequelize.STRING
+      },
+      followers: {
+        type: Sequelize.STRING
+      },
+      following: {
+        type: Sequelize.STRING
+      },
+      likes: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require('express');
-const userRoutes = require('./routes/userRoutes');
+const bodyParser = require('body-parser');
 require('dotenv').config({ path: './config/.env' });
 const app = express();
+const { sequelize, User, Post } = require("./models");
 
-//routes
-app.use('/api/user', userRoutes);
 
-// server
-app.listen(process.env.PORT, () => {
-    console.log(`Listening on port ${process.env.PORT}`);
+//---------------------- Middleware général : Transforme corps de la requête en obj JS utilisable
+app.use(bodyParser.json());
 
+app.post('/users', async(req, res) => {
+    const { firstname, lastname, username, email, password } = req.body
+    try {
+        const user = await User.create({ firstname, lastname, username, email, password })
+        return res.json(user)
+    } catch(err) {
+        console.log(err)
+        return res.status(500).json({ message: err })
+    }
+});
+
+// Connexion server + Sequelize
+app.listen(process.env.PORT, async () => {
+    try {
+        console.log(`Listening on port ${process.env.PORT}`),
+        await sequelize.sync({ force: true });
+        console.log("Connection has been established successfully.");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    } 
 });
