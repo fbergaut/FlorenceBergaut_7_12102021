ed87d1564f3941aafc7036918c6bcdbeff1f1370
fbergaut <f.bergaut@gmail.com>
2021-10-20 09:45:37 +0200

    Ajout de UUID au model user + recréation de la db + migration

--- migrations/20211018140340-create-user.js
+++ migrations/20211018140340-create-user.js
@@ -8,6 +8,10 @@ module.exports = {
                 primaryKey: true,
                 type: DataTypes.INTEGER
             },
+            uuid: {
+                type: DataTypes.UUID,
+                defaultValue: DataTypes.UUIDV4
+            },
             firstname: {
                 type: DataTypes.STRING,
                 allowNull: false
--- models/user.js
+++ models/user.js
@@ -1,58 +1,66 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init(
-    {
-    firstname: { 
-      type:DataTypes.STRING,
-      allowNull: false
-      },
-    lastname: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    username: { 
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: { 
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    picture: { 
-      type: DataTypes.STRING,
-      defaultValue: "./uploads/profil/random-user.png"
-    },
-    bio: {
-      type: DataTypes.STRING(800),
-    },
-    followers: {
-      type: DataTypes.STRING,
-    },
-    following: { 
-      type: DataTypes.STRING,
-    },
-    likes: {
-      type: DataTypes.STRING
-      }
-  }, {
-    sequelize,
-    tableName: 'users',
-    modelName: 'User',
-  });
-  return User;
+    class User extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+
+        // fonction qui permet de cacher l'id en retour au user
+        toJSON() {
+            return {...this.get(), id: undefined }
+        }
+    };
+    User.init({
+        uuid: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4
+        },
+        firstname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        lastname: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        picture: {
+            type: DataTypes.STRING,
+            defaultValue: "./uploads/profil/random-user.png"
+        },
+        bio: {
+            type: DataTypes.STRING(800),
+        },
+        followers: {
+            type: DataTypes.STRING,
+        },
+        following: {
+            type: DataTypes.STRING,
+        },
+        likes: {
+            type: DataTypes.STRING
+        }
+    }, {
+        sequelize,
+        tableName: 'users',
+        modelName: 'User',
+    });
+    return User;
 };
