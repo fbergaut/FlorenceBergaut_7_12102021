e2a65f69d1bf020a5b721df66f7b700485c56923
fbergaut <f.bergaut@gmail.com>
2021-11-25 09:36:24 +0100

    Handle html part of UploadImg component

--- frontend/src/components/Profil/UploadImg.jsx
+++ frontend/src/components/Profil/UploadImg.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 const UploadImg = () => {
+    const [file, setFile] = useState();
+
+    const handlePicture = (e) => {
+        e.preventDefault();
+    }
 
     return (
-        <div>
-            upload
-        </div>
+        <form action="" onSubmit={handlePicture} className="upload-pic">
+            <label htmlFor="file">Changer d'image</label>
+            <input type="file" id="file" name="file" accept=".jpg, .jpeg, .png" onChange={(e) => setFile(e.target.files[0])}/>
+            <br />
+            <input type="submit" value="Envoyer" />
+        </form>
     )
 };
 
