48a57382a597157179df3f9535a8ff4eee75a00a
fbergaut <f.bergaut@gmail.com>
2021-12-10 09:56:05 +0100

    Add a loading spinner in cards display to prevent user data is loading

--- frontend/src/components/Post/Card.jsx
+++ frontend/src/components/Post/Card.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { isEmpty } from "../Utils";
 
-const Card = ()=> {
+const Card = ({ post })=> {
+    const [isLoading, setIsLoading] = useState(true);
+    const usersData = useSelector((state) => state.usersReducer);
+    const userData = useSelector((state) => state.userReducer);
+
+    useEffect(() => {
+        !isEmpty(usersData[0]) && setIsLoading(false);
+    }, [usersData])
+
     return (
-        <div>
-            Card
-        </div>
+        <li className="card-container" key={post.uuid}>
+            {isLoading ? (
+                <i className='fas fa-spinner fa-spin'></i>
+            ) : (
+                <h2>test</h2>
+            )}
+        </li>
     );
 };
 
