4e363488c4726c8e151c0d2f9ba03984cfda7468
fbergaut <f.bergaut@gmail.com>
2021-12-15 10:45:22 +0100

    Main part of card finished

--- frontend/src/components/Post/Card.jsx
+++ frontend/src/components/Post/Card.jsx
@@ -68,7 +68,10 @@ const Card = ({ post })=> {
                             <img src="./img/icons/message1.svg" alt="comment" />
                             <span>{post.comments.length}</span>
                         </div>
+                        <h6>Like button</h6>
+                        <img src="./img/icons/share.svg" alt="share" />
                     </div>
+                    
                 </div>
                 </>
             )}
