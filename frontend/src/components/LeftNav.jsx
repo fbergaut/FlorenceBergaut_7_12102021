8cc4ff2d0d98c4a0b8eb908b8b7349ad4fcbfca4
fbergaut <f.bergaut@gmail.com>
2022-01-11 16:09:38 +0100

    Fix a className issue

--- frontend/src/components/LeftNav.jsx
+++ frontend/src/components/LeftNav.jsx
@@ -7,12 +7,12 @@ const LeftNav = () => {
             <div className="icons">
                 <div className="icons-bis">
                     <NavLink to='/' exact activeClassName="active-left-nav">
-                        <i class="fas fa-home"></i>
+                        <i className="fas fa-home"></i>
                         {/* <img src="./img/icons/home.svg" alt="home" /> */}
                     </NavLink>
                     <br/>
                     <NavLink to='/profil' exact activeClassName="active-left-nav">
-                        <i class="fas fa-user-circle"></i>
+                        <i className="fas fa-user-circle"></i>
                         {/* <img src="./img/icons/user.svg" alt="user" /> */}
                     </NavLink>
                 </div>
